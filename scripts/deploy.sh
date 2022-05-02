90b0626ed6842b9b2c3ed4d1f032b369aa5f9bd0
dvdsk <noreply@davidsk.dev>
2022-05-02 15:18:48 +0200

    fix deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,13 +5,15 @@ BUILD_ARG=$1
 SERVER="sgc"  # ssh config name or full adress
 
 RELEASE=debug
-if [[ BUILD_ARG == "--release" ]]; then
+if [[ $BUILD_ARG == "--release" ]]; then
 	RELEASE=release
 fi
 
 # update hugo site
 (cd public && hugo -D)
 rsync -r public/public sgc:/tmp/
+rsync -r forwarded.txt sgc:/tmp/
+
 
 cross build --target=aarch64-unknown-linux-gnu $BUILD_ARG
 rsync -vh --progress \
@@ -22,6 +24,7 @@ cmds="
 sudo rm -rf /home/webserver/files /home/webserver/webserver
 sudo mkdir /home/webserver/files
 sudo mv /tmp/public /home/webserver/files/
+sudo mv /tmp/forwarded.txt /home/webserver/forwarded.txt
 sudo mv /tmp/webserver /home/webserver/webserver
 sudo chown -R webserver:webserver /home/webserver/
 sudo systemctl restart webserver.service
