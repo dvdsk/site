5ccb8543891a349edd25bcfed658f69c419bf4a2
dvdsk <noreply@davidsk.dev>
2022-05-02 14:26:12 +0200

    adds url forwarding from file

---
+++ src/forwarder.rs
@@ -0,0 +1,84 @@
+use std::collections::HashMap;
+use std::convert::TryFrom;
+use std::fs;
+use std::sync::{Arc, RwLock};
+
+use actix_web::http::{header, Uri};
+use actix_web::{web, HttpResponse};
+use color_eyre::{eyre::eyre, eyre::WrapErr, Result};
+use itertools::Itertools;
+use log::{error, info};
+
+#[derive(Debug, Clone)]
+pub struct ForwardingTable(Arc<RwLock<HashMap<String, String>>>);
+
+fn url_pairs(line: &str) -> Result<(String, String)> {
+    let (from, to) = line
+        .split_once(" ")
+        .ok_or_else(|| eyre!("Could not split line: {line}"))?;
+    let _ = Uri::try_from(from).wrap_err_with(|| format!("Could not parse into Uri: {from}"))?;
+    let _ = Uri::try_from(to).wrap_err_with(|| format!("Could not parse into Uri: {to}"))?;
+    Ok((from.to_owned(), to.to_owned()))
+}
+
+/// spawns background function reloading config as needed
+impl ForwardingTable {
+    const PATH: &'static str = "forwarded.txt";
+
+    fn setup_reload_on_change(self) -> Result<()> {
+        use hotwatch::{Event, Hotwatch};
+        let mut hotwatch = Hotwatch::new().wrap_err("Could not enable live reloading")?;
+
+        hotwatch
+            .watch(Self::PATH, move |event: Event| {
+                if let Event::Write(path) = event {
+                    info!("reloading url forwarding from: {path:?}");
+                    if let Err(e) = self.reload() {
+                        error!("ran into err reloading: {e:?}");
+                    }
+                }
+            })
+            .wrap_err("failed to watch file")
+    }
+
+    fn reload(&self) -> Result<()> {
+        let content =
+            fs::read_to_string(Self::PATH).wrap_err("could not open file: {Self::PATH}")?;
+
+        let (new_map, errs): (HashMap<_, _>, Vec<_>) = content
+            .lines()
+            .skip_while(|s| s.starts_with("#"))
+            .skip_while(|s| s.is_empty())
+            .map(url_pairs)
+            .partition_result();
+        for err in errs {
+            error!("{err:?}");
+        }
+
+        let mut writer = self.0.write().unwrap();
+        *writer = new_map;
+        Ok(())
+    }
+
+    pub fn init() -> Self {
+        let table = HashMap::new();
+        let table = Self(Arc::new(RwLock::new(table)));
+        if let Err(e) = table.reload() {
+            error!("{e:?}")
+        }
+        if let Err(e) = table.clone().setup_reload_on_change() {
+            error!("{e:?}");
+        }
+        table
+    }
+}
+
+pub async fn route(data: web::Data<ForwardingTable>, path: web::Path<String>) -> HttpResponse {
+    let table = data.0.read().unwrap();
+    match dbg!(table.get(&path.into_inner())) {
+        Some(forward_to) => HttpResponse::TemporaryRedirect()
+            .append_header((header::LOCATION, forward_to.as_str()))
+            .finish(),
+        None => HttpResponse::NotFound().finish(),
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,52 @@
+use actix_web::web::Data;
 use structopt::StructOpt;
-
 use actix_files::Files;
-use actix_web::{middleware, App, HttpServer};
+use actix_web::{middleware, web, App, HttpServer};
+
+mod forwarder;
 
 mod errors;
 use errors::LogLevel;
 
+use self::forwarder::ForwardingTable;
+
 #[derive(Debug, StructOpt)]
-#[structopt(name="webserver")]
-/// Personal page backend, shows either a dashboard if logged in or 
+#[structopt(name = "webserver")]
+/// Personal page backend, shows either a dashboard if logged in or
 /// a static site generated using Hugo
 struct Opt {
     /// port on which this server will listen
-    #[structopt(long, default_value = "Critical")]
+    #[structopt(long)]
     port: u16,
-    /// log level least, one of: Critical,
+    /// log level least, one of: Critical, Info
     #[structopt(long, default_value = "Critical")]
     verbosity: LogLevel,
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    color_eyre::install().unwrap();
+
     let opt = Opt::from_args();
     errors::setup_logging(&opt.verbosity).unwrap();
 
-    HttpServer::new(|| {
+    let forwarder = ForwardingTable::init();
+    let data = Data::new(forwarder);
+
+    HttpServer::new(move || {
         App::new()
+            .app_data(Data::clone(&data))
             // Enable the logger.
             .wrap(middleware::Logger::default())
+            .service(
+                web::resource("/{a}")
+                    // .name("url shortner")
+                    // .guard(guard::Get())
+                    .to(forwarder::route),
+            )
             .service(Files::new("/", "./files/public/").index_file("index.html"))
     })
-    .bind(format!("127.0.0.1:{}",opt.port))?
+    .bind(format!("127.0.0.1:{}", opt.port))?
     .run()
     .await
 }
